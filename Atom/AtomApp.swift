1015ef51ba07d5e160c512cc08847c95caff929b
Sam Quan <sammiequan@gmail.com>
2020-12-28 15:06:12 -0800

    Initial Commit

---
+++ Atom/AtomApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomApp.swift
+//  Atom
+//
+//  Created by Sam Quan on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct AtomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
