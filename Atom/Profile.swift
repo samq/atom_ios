1f120c4ace6b010ad900aecc8617ad4722bf48cb
Sam Quan <sammiequan@gmail.com>
2021-01-18 09:33:13 -0800

    Add header and user information views to the Profile screen.

--- Atom/Profile.swift
+++ Atom/Profile.swift
@@ -9,7 +9,63 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
-        Text("Profile - Screen")
+        NavigationView {
+            VStack {
+                // Header
+                HStack {
+                    Text("My Profile")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    Text("Help")
+                        .font(.system(size: 14))
+                        .foregroundColor(.blue)
+                    Image(systemName: "gearshape")
+                }
+                .padding(.vertical, 4)
+                // Content
+                ScrollView {
+                    // User Information
+                    HStack {
+                        // Profile Image - Placeholder
+                        Image(systemName: "person.circle")
+                            .font(.system(size: 48))
+                        // Name, Phone Number, Email
+                        VStack(alignment: .leading) {
+                            Text("John Doe")
+                                .foregroundColor(.white)
+                            Text("+1 234-567-8910")
+                            Text("johndoe@gmail.com")
+                        }
+                        .foregroundColor(.gray)
+                        .padding(.horizontal, 4)
+                        Spacer()
+                        // Edit Option
+                        // TODO: Add option to edit current user information
+                        Text("Edit Info")
+                            .font(.system(size: 14))
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.vertical)
+                    
+                    // My Tickets and Invites
+                    
+                    
+                    // My Watch List
+                    
+                    
+                    // Movies I've Seen
+                    
+                    
+                    // My Connected Accounts
+                    // Google / Facebook / Amazon / Snapchat
+                }
+            }
+            .navigationBarHidden(true)
+            .padding()
+            .background(Color.black)
+            .foregroundColor(.white)
+        }
     }
 }
 
